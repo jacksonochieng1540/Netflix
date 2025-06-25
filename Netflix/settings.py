4d89fa7419e3dab4082aa1ff512fcc4da038aee4
ayagah <ayagajack@gmail.com>
2025-06-25 12:21:09 +0300

    corrected the CSRF_TRUSTED_ORIGINS

--- Netflix/settings.py
+++ Netflix/settings.py
@@ -137,7 +137,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS='*'
+#CSRF_TRUSTED_ORIGINS='*'
 CSRF_COOKIE_SECURE=True
 SESSION_COOKIE_SECURE=True
 SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO','https')
